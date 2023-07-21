0c672602658bdb3f5c94f5f07fc46fdf1d740975
Paras <paras@scus.tech>
2023-07-21 18:42:31 +0530

    Changes push

---
+++ src/api/card-details/controllers/card-details.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * card-details controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::card-details.card-details');
---
+++ src/api/card-details/services/card-details.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * card-details service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::card-details.card-details');

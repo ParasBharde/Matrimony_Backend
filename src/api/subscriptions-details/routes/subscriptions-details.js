3bbd0f21a36bd33791a1e5de182e00263f7ec4fe
Paras <paras@scus.tech>
2023-08-11 20:55:07 +0530

    Order_History Collection Created

--- src/api/subscriptions-details/routes/subscriptions-details.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * subscriptions-details router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::subscriptions-details.subscriptions-details');
--- src/api/subscriptions-details/services/subscriptions-details.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * subscriptions-details service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::subscriptions-details.subscriptions-details');
